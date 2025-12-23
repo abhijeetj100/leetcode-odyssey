d8c9f9a9265c04a3463319166f8d6f29829a90bd
Abhijeet <abhijeetjain456@gmail.com>
2025-12-23 12:10:47 +0530

    Time: 2 ms (95.46%), Space: 43.8 MB (13.87%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        Arrays.sort(costs, (a, b) -> (a[0]-a[1])-(b[0]-b[1]));
+
+        int n = costs.length/2;
+
+        int cost = 0;
+
+        for(int i=0; i<n; i++){
+            cost+=costs[i][0];
+            cost+=costs[i+n][1];
+        }
+
+        return cost;
+    }
+}
