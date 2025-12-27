84a77199e7ecc415fa8bf3f9aec5d2f09c0d1bfd
Abhijeet <abhijeetjain456@gmail.com>
2025-12-27 21:10:53 +0530

    Time: 0 ms (100%), Space: 43.1 MB (12.66%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dp = new int[101];
+    public int rob(int[] nums) {
+        Arrays.fill(dp, -1);
+        return solve(nums, nums.length-1);
+    }
+
+    public int solve(int[] nums, int n){
+        if(n<0) return 0;
+        if(n==0) return nums[0];
+        if(dp[n] != -1) return dp[n];
+
+        dp[n] = Math.max(solve(nums, n-2)+nums[n], solve(nums, n-1));
+        return dp[n];
+    }
+}
